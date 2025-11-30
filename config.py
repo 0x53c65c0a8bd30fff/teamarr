0db09e75750a9c0f79005f093f588dac172dac84
egyptiangio <minazaki@gmail.com>
2025-11-30 16:58:39 -0500

    Bump version to 1.2.1

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 # Application version - single source of truth
 # Format: MAJOR.MINOR.PATCH[-pre-release][+build]
-BASE_VERSION = "1.2.0"
+BASE_VERSION = "1.2.1"
 
 def get_version():
     """
